8c0aac4d3fe0070888755ba546f158fb1ff16245
name <name@gmail.com>
2021-04-25 15:33:26 +0700

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="containers">
+      <Head>
+        <title>Karmania</title>
+      </Head>
+      <img src="" alt="" />
+    </div>
+  );
+}
