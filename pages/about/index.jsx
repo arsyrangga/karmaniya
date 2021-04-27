b4728003eb50c899e9510f3bd65049cd3857c13f
sandyiswayudi <siswahyudi45@gmail.com>
2021-04-27 17:38:30 +0700

    kamania update - about

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -15,6 +15,50 @@ const Index = () => {
         ></link>
     </Head>
     <Navbar />
+                                 {/* konten */}
+    <div className="mainAbout">
+
+      {/* judul about */}
+      <div className="judul">
+        <hr className="hrLeft"/>
+        <h3 className="about">About Us</h3>
+        <hr className="hrRight"/>
+      </div>
+      {/* END judul about */}
+
+      {/* gambar1 */}
+      <div className="gambar1">
+        <div className="tesGambar">
+          <p>halo</p>
+        </div>
+        {/* <img src="/assets/kamaniya.png" alt="img1" width="1000px"/> */}
+        <div className="tulisan">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorem non tempore dolores perferendis sed saepe nihil quidem ipsam, velit dolore, ea obcaecati aperiam qui porro! Quis similique culpa facilis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit deserunt hic obcaecati illo neque ipsam veniam optio earum quaerat blanditiis ipsum aperiam iusto eligendi voluptatum eos nihil, quod sit iure.</p>
+      <div className="tulisan2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorem non tempore dolores perferendis sed saepe nihil quidem ipsam, velit dolore, ea obcaecati aperiam qui porro! Quis similique culpa facilis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit deserunt hic obcaecati illo neque ipsam veniam optio earum quaerat blanditiis ipsum aperiam iusto eligendi voluptatum eos nihil, quod sit iure.</p>
+      </div>
+      </div>
+
+      </div>
+      {/* END gambar1 */}
+
+      {/* gambar2 */}
+      <div className="gambar1">
+        <div className="tesGambar">
+          <p>halo</p>
+        </div>
+        {/* <img src="/assets/kamaniya.png" alt="img1" width="1000px"/> */}
+        <div className="tulisan">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorem non tempore dolores perferendis sed saepe nihil quidem ipsam, velit dolore, ea obcaecati aperiam qui porro! Quis similique culpa facilis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit deserunt hic obcaecati illo neque ipsam veniam optio earum quaerat blanditiis ipsum aperiam iusto eligendi voluptatum eos nihil, quod sit iure.</p>
+      <div className="tulisan2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorem non tempore dolores perferendis sed saepe nihil quidem ipsam, velit dolore, ea obcaecati aperiam qui porro! Quis similique culpa facilis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit deserunt hic obcaecati illo neque ipsam veniam optio earum quaerat blanditiis ipsum aperiam iusto eligendi voluptatum eos nihil, quod sit iure.</p>
+      </div>
+      </div>
+
+      </div>
+
+      </div>
+                                 {/* END konten */}
   </div>
   );
 };
