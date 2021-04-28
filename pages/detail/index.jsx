2cf2d6415a0ecd55c74fa74ddff370c023a589ad
name <name@gmail.com>
2021-04-28 22:48:53 +0700

    rangga

--- pages/detail/index.jsx
+++ pages/detail/index.jsx
@@ -2,24 +2,25 @@ import Footer from "../../components/footer/Footer";
 import "./Detail.css";
 import Head from "next/head";
 import Navbar from "../../components/navbar/Navbar";
+import Detail from "../../components/detail/Detail";
 
 const Index = () => {
-    return (
-        <div className="detail">
-            <Head>
-                <title>Karmania</title>
-                <link
-                rel="stylesheet"
-                href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
-                integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
-                crossorigin="anonymous"
-                ></link>
-            </Head>
-            <Navbar />
-            <Detail />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="detail">
+      <Head>
+        <title>Karmania</title>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+          integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
+          crossorigin="anonymous"
+        ></link>
+      </Head>
+      <Navbar />
+      <Detail />
+      <Footer />
+    </div>
+  );
 };
 
 export default Index;
