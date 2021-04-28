a20af65e53d408d2d5a56d3954db3ce2af2c0268
name <name@gmail.com>
2021-04-28 22:27:07 +0700

    update rangga

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,47 +1,99 @@
 import "./Navbar.css";
-import React from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [show, setshow] = useState(false);
   return (
     <nav className="navbar">
-      <div className="menu-toggle">
-          <input type="checkbox"/>
-          <span></span>
-          <span></span>
-          <span></span>
+      <div
+        className="menu-toggle"
+        onClick={() => {
+          setshow(true);
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-      <ul className="menu-list">
-          <li><a href="">HOME</a></li>
-          <li><a href="">PROMO</a></li>
-          <li><a href="">SERVICE</a></li>
-          <li><a href="">RETAIL PRODUCTS</a></li>
-          <li><a href="">STORY</a></li>
-          <li><a href="">ABOUT US</a></li>
-          <li><a href="">CONTACT</a></li>
-          <li><a href="">LOCATION</a></li>
-      </ul>
+
+      {show && (
+        <ul className="menu-list">
+          <div className="icncontainer">
+            <i
+              className="fas fa-times closeicon"
+              onClick={() => {
+                setshow(false);
+              }}
+            ></i>
+          </div>
+          <li>
+            <a href="">HOME</a>
+          </li>
+          <li>
+            <a href="">PROMO</a>
+          </li>
+          <li>
+            <a href="">SERVICE</a>
+          </li>
+          <li>
+            <a href="">RETAIL PRODUCTS</a>
+          </li>
+          <li>
+            <a href="">STORY</a>
+          </li>
+          <li>
+            <a href="">ABOUT US</a>
+          </li>
+          <li>
+            <a href="">CONTACT</a>
+          </li>
+          <li>
+            <a href="">LOCATION</a>
+          </li>
+        </ul>
+      )}
+
       <div className="logo">
-          <h3><a href=""><img src="./assets/kamaniyalogo.png" alt="Kamaniya"/></a></h3>
+        <h3>
+          <a href="">
+            <img src="./assets/kamaniyalogo.png" alt="Kamaniya" />
+          </a>
+        </h3>
       </div>
       <ul className="menu">
-          <li>
-              <form action="" method="GET">
-                  <input type="search" id="search" placeholder="Search" className="search_text"/>
-                  <label htmlFor="serach"><i class="fas fa-search"></i></label>
-              </form>
-          </li>
-          <li>
-            <a href="https://macam-macamperalatan.surge.sh"><i class="fab fa-facebook-f"></i></a>
-          </li>
-          <li>
-            <a href="https://macam-macamperalatan.surge.sh"><i class="fab fa-instagram"></i></a>
-          </li>
-          <li>
-            <a href="https://macam-macamperalatan.surge.sh"><img src="/assets/Tokopedia.png" alt="Tokopedia"/></a>
-          </li>
-          <li>
-            <a href="" className="btn-submit" type="submit">RESERVER NOW</a>
-          </li>
+        <li>
+          <form action="" method="GET">
+            <input
+              type="search"
+              id="search"
+              placeholder="Search"
+              className="search_text"
+            />
+            <label htmlFor="serach">
+              <i class="fas fa-search"></i>
+            </label>
+          </form>
+        </li>
+        <li>
+          <a href="https://macam-macamperalatan.surge.sh">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://macam-macamperalatan.surge.sh">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://macam-macamperalatan.surge.sh">
+            <img src="/assets/Tokopedia.png" alt="Tokopedia" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="btn-submit" type="submit">
+            RESERVER NOW
+          </a>
+        </li>
       </ul>
     </nav>
   );
