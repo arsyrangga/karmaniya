22aa7d38a8631c31d560a11a65b686b3dfa5c66d
name <name@gmail.com>
2021-07-15 21:04:43 +0700

    teh

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -4,10 +4,11 @@ import React, { useState } from "react";
 const Navbar = () => {
   const [show, setshow] = useState(false);
   return (
+    <>
     <nav className="navbar">
       <div className="navbar-left"><img src="/assets/kamaniyalogo.png" alt="" /></div>
       <div className="navbar-right">
-        <i className="fas fa-bars"></i>
+        <i className="fas fa-bars primary" onClick={()=>setshow(!show)} ></i>
         <a href="/">Home</a>
         <a href="/services">Service</a>
         <a href="/retail-product">Retail Products</a>
@@ -16,6 +17,17 @@ const Navbar = () => {
         <a href="/location">Contact</a>
       </div>
     </nav>
+
+  <div className="navbar-mobile primary" style={{display: "none", transform : show ? "translateY(-90vh)" : "translateY(0)"}}>
+      <a href="/">Home</a>
+      <a href="/services">Service</a>
+      <a href="/retail-product">Retail Products</a>
+      <a href="/about">About</a>
+      <a href="/story">Story</a>
+      <a href="/location">Contact</a>
+  </div>
+    
+    </>
   );
 };
 
